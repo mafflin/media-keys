84323e556f941c25a252e5ce6d2813490b8f2088
Siarhei Ivanouski <siarhei.ivanouski@gmail.com>
2025-03-16 10:38:40 +0100

    Clean up

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const St = imports.gi.St
 const Main = imports.ui.main
@@ -8,42 +8,43 @@ const Util = imports.misc.util
 const executeAction = (action) => Util.spawn(['playerctl', action])
 
 const PANEL_POSITON = 2
-const BUTTONS = {
-  next: {
+const ICON_STYLE_CLASS = 'system-status-icon'
+const BUTTON_STYLE_CLASS = 'panel-button'
+const BUTTONS = [
+  {
     icon: 'media-skip-forward-symbolic',
-    action: () => executeAction('next'),
+    action: 'next',
   },
-  pause: {
+  {
     icon: 'media-playback-pause-symbolic',
-    action: () => executeAction('play-pause'),
+    action: 'play-pause',
   },
-  prev: {
+  {
     icon: 'media-skip-backward-symbolic',
-    action: () => executeAction('previous'),
+    action: 'previous',
   },
-}
+]
 
-const buttons = Object.keys(BUTTONS).map((key) => {
-  const button = BUTTONS[key]
-  return {
-    ...button,
-    element: new St.Button({ style_class: 'panel-button' }),
-    icon: new St.Icon({
-      style_class: 'system-status-icon',
-      icon_name: button.icon,
-    }),
-  }
-})
+const buttons = BUTTONS.map(({ action, icon }) => ({
+  action,
+  element: new St.Button({ style_class: BUTTON_STYLE_CLASS }),
+  icon: new St.Icon({
+    style_class: ICON_STYLE_CLASS,
+    icon_name: icon,
+  }),
+}))
 
 function init() {
   buttons.forEach((button) => {
     button.element.set_child(button.icon)
-    button.element.connect('button-press-event', button.action)
+    button.element.connect('button-press-event', () => executeAction(button.action))
   })
 }
 
 function enable() {
-  buttons.forEach(({ element }) => Main.panel._rightBox.insert_child_at_index(element, PANEL_POSITON))
+  buttons.forEach(({ element }) =>
+    Main.panel._rightBox.insert_child_at_index(element, PANEL_POSITON),
+  )
 }
 
 function disable() {
